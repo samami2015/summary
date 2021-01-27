d2a4107e8d4e1bb43982bc7c7374809c995caa5f
wangsen <22232647@qq.com>
2021-01-27 21:18:33 +0800

    Add HJ056,HJ058,HJ059 files

---
+++ src/HJ056.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class HJ056 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = "";
+        while ((str = br.readLine()) != null) {
+            System.out.println(count(Integer.valueOf(str)));
+        }
+    }
+
+    private static int count(int n) {
+        int result = 0;
+        for (int i = 1; i < n; i++) {
+            int sum = 0;
+            for (int j = 1; j <= i / 2; j++) {
+                if (i % j == 0) {
+                    sum += j;
+                }
+            }
+            if (sum == i) {
+                result++;
+            }
+        }
+        return result;
+    }
+}
