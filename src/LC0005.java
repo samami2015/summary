460c9f19db21689b3a5e619daf75aa219fd7283a
wangsen <22232647@qq.com>
2021-02-01 22:51:04 +0800

    Add LC0003,LC0005,LC0006 files

---
+++ src/LC0005.java
@@ -0,0 +1,29 @@
+public class LC0005 {
+    public String longestPalidrome(String s) {
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        String ans = "";
+        for (int l = 0; l < n; ++l) {
+            for (int i = 0; i + l < n; ++i) {
+                int j = i + l;
+                if (l == 0) {
+                    dp[i][j] = true;
+                } else if (l == 1) {
+                    dp[i][j] = (s.charAt(i) == s.charAt(j));
+                } else {
+                    dp[i][j] = (s.charAt(i) == s.charAt(j) && dp[i + 1][j - 1]);
+                }
+                if (dp[i][j] && l + 1 > ans.length()) {
+                    ans = s.substring(i, i + l + 1);
+                }
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        LC0005 solution = new LC0005();
+        String ret = solution.longestPalidrome("babad");
+        System.out.println(ret);
+    }
+}
